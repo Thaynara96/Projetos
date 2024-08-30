2f45dcfecf45e6a6e88fdf6a0a587da98eefe28c
Thaynara Pimentel <thaynarapimentel@gmail.com>
2024-08-30 14:42:58 -0300

    inclusão

---
+++ App_mata_mosquito/jogo.js
@@ -0,0 +1,73 @@
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+//função necessario pois a imagem está no body e o arquivo js estão no head, sendo assim aparece erro
+
+function posicaoRandomica(){
+    // variavel para que o mosquito apareça dentro do limite da pagina
+    // -90 é para que a imagem não passe do limite
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) -90
+
+    // Caso a posição x e y forem menor que 0 não vai diminuir os 90, se diminuir a imagem some da tela
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    // criar o elemento html para que o mosquito apareça em partes aleatorias da pagina
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    // é necessario colocar um espaço entre as funções para que o navegador entenda que são duas funções
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+
+}
+
+// função para que o mosquito apareça de tamanhos variados
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch (classe){
+        case 0:
+            return 'mosquito1'
+        
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+
+}
+
+// função para o mosquito aparecer virado para esquerda ou direia de forma aleatoria
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2 )
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    }
+}
